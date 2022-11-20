195b21fbbf7869783ad60c661c8f58774fc83fc8
Martin Pellizzer <martinpellizzer@gmail.com>
2022-11-20 18:37:46 +0100

    -:

---
+++ main.py
@@ -0,0 +1,20 @@
+import jinja2
+import pdfkit
+from datetime import datetime
+
+demo = datetime.today().strftime('%d %b, %Y')
+
+value = '7 easy charts'
+
+context = {
+    'value': value,
+}
+
+template_loader = jinja2.FileSystemLoader('./')
+template_env = jinja2.Environment(loader=template_loader)
+
+template = template_env.get_template('template.html')
+output_text = template.render(context)
+
+config = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+pdfkit.from_string(output_text, 'output.pdf', configuration=config, css='style.css')
